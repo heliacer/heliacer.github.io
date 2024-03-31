ed555eda175c655bd2f01f086560b27db46634f8
heliacer <heliacer35@gmail.com>
2024-03-31 10:56:14 +0200

    Changes to be committed: 	modified:   index.html 	modified:   misc/main.css 	modified:   misc/main.js 	new file:   misc/typewriter.js
    Changes not staged for commit:
    	modified:   misc/main.css

--- misc/main.js
+++ misc/main.js
@@ -1,29 +1,9 @@
-handleNavigation()
-window.addEventListener("hashchange", () => handleNavigation());
 function handleNavigation() {
   if (window.location.hash){
     document.querySelectorAll('.navitem a').forEach(link => { link.classList.remove('active') });
     document.querySelector(`.navitem a[href="${window.location.hash}"]`).classList.add('active');
   }
 }
-
-window.addEventListener('scroll', () => {
-  const topics = document.querySelectorAll('.topic');
-  topics.forEach(topic => {
-      const rect = topic.getBoundingClientRect();
-      const topIsVisible = rect.bottom >= 0 && rect.top <= window.innerHeight;
-      const bottomIsVisible = rect.top <= window.innerHeight && rect.bottom >= 0;
-      const fullyVisible = topIsVisible && bottomIsVisible && rect.top >= 0 && rect.bottom <= window.innerHeight;
-
-      if (fullyVisible) {
-          if (window.location.hash !== `#${topic.id}`) {
-              history.replaceState(null, null, `#${topic.id}`);
-              handleNavigation();
-          }
-      }
-  });
-});
-
 
 
 document.querySelectorAll('.navitem a[href^="#"]').forEach(anchor => {
@@ -36,12 +16,51 @@ document.querySelectorAll('.navitem a[href^="#"]').forEach(anchor => {
 
     if (target) {
       const offsetPosition = target.getBoundingClientRect().top + window.scrollY - navbarHeight;
+      isScrollingByUser = false;
       window.scrollTo({
         top: offsetPosition - 30,
         behavior: 'smooth',
         duration: 500
       });
-      handleNavigation();
+      handleNavigation()
     }
   });
+});
+
+window.addEventListener('scroll', () => {
+  const topics = document.querySelectorAll('.topic');
+  const options = [];
+
+  topics.forEach(topic => {
+    const rect = topic.getBoundingClientRect();
+    options.push({ id: topic.id, distanceToZero: Math.abs(rect.top / rect.bottom) });
+  });
+  let nearestIndex = 0;
+  let nearestDistance = options[0].distanceToZero;
+  for (let i = 1; i < options.length; i++) {
+    if (options[i].distanceToZero < nearestDistance) {
+      nearestIndex = i;
+      nearestDistance = options[i].distanceToZero;
+    }
+  }
+  const nearestId = options[nearestIndex].id;
+  history.replaceState(null, null, `#${nearestId}`);
+  handleNavigation();
+});
+
+
+window.addEventListener('load', () => {
+  if (window.location.hash) {
+    const targetId = window.location.hash.substring(1);
+    const target = document.getElementById(targetId);
+    const navbarHeight = document.querySelector('nav').offsetHeight;
+    if (target) {
+      const offsetPosition = target.getBoundingClientRect().top + window.scrollY - navbarHeight;
+      window.scrollTo({
+        top: offsetPosition - 30,
+        behavior: 'auto'
+      });
+    }
+    handleNavigation()
+  }
 });
