701e1eba46196364392b2ca83a793d20b3b6c2a2
heliacer <heliacer35@gmail.com>
2024-05-26 21:22:35 +0200

    Init react

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import './styles/App.css';
+import Navbar from './ui/navbar';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
+
+function Topic() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+  
+  return (
+    <div className="topic" data-aos="fade-up">
+      <h1>Topic</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima laboriosam illum exercitationem ullam, similique, eos quis repudiandae harum nesciunt quibusdam maiores impedit? Repudiandae ipsum esse, eligendi dolores nisi est officiis.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores nihil quidem praesentium consectetur amet dolor tempora nostrum rerum ipsa sed voluptate officia, obcaecati ducimus laboriosam vero iste officiis magni itaque!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam tempora molestias suscipit quo mollitia fuga cupiditate eligendi ab architecto magni accusamus sequi minus ipsa deleniti nisi illum reiciendis, repellendus obcaecati.</p>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <p>Work in Progress</p>
+      <Topic />
+      <Topic />
+      <Topic />
+      <Topic />
+      <Topic />
+      <Topic />
+      <Topic />
+      <Topic />
+    </>
+  );
+}
