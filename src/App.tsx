667ab11fabf22e01a556284c8a9dea529556e9e7
heliacer <heliacer35@gmail.com>
2024-05-29 22:05:01 +0200

    footer + mobile adjustment

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import Navigation from './ui/navigation';
-import { GlowBallRows } from './ui/misc';
+import { GlowBallRows } from './ui/globall';
 import { useEffect, useState } from 'react';
 import Hero from './ui/hero';
 import Topic from './ui/topic';
 import { about, backend_expertise, frontend_expertise } from './assets/text';
+import Footer from './ui/footer';
 
 export default function App() {
   const [onTop, setOnTop] = useState(true);
@@ -21,7 +22,7 @@ export default function App() {
   return (
     <>
       <Navigation className={onTop ? '' : 'scrolled'}/>
-      <GlowBallRows count={6} distance={1000} radius={100} />
+      <GlowBallRows count={4} distance={1000} radius={100} />
       <div className='content'>
         <Hero />
         <Topic name='about'>
@@ -40,6 +41,7 @@ export default function App() {
           {about}
         </Topic>
       </div>
+      <Footer />
     </>
   );
 }
---
+++ src/ui/footer.tsx
@@ -0,0 +1,23 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDiscord, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>© 2024 Heliacer</p>
+      <a onClick={() => window.open('https://github.com/heliacer')}>
+        <FontAwesomeIcon icon={faGithub}/>
+        <p className="pc">Github</p>
+      </a>
+      <a onClick={() => window.open('https://discord.gg/DA4mBJX8nb')}>
+        <FontAwesomeIcon icon={faDiscord}/>
+        <p className="pc">My Community</p>
+      </a>
+      <a onClick={() => window.open('mailto:heliacer35@gmail.com')}>
+        <FontAwesomeIcon icon={faEnvelope}/>
+        <p className="pc">Email Me</p>
+      </a>
+    </footer>
+  );
+}
